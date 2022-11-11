546d6bf3f0218c161ef469e485523b8cbc987ee1
fakhryhizballah <falehry88@gmail.com>
2022-11-11 10:46:27 +0700

    report Ralan

---
+++ model/ralan.js
@@ -0,0 +1,122 @@
+const { con } = require('./index.js');
+var getLaporan = async function (from, until) {
+    var sql = `
+	SELECT
+	reg_periksa.no_rawat AS no_rawat, 
+	reg_periksa.tgl_registrasi AS tgl_registrasi, 
+	reg_periksa.jam_reg AS jam_reg, 
+	reg_periksa.no_rkm_medis AS no_rkm_medis, 
+	pasien.no_ktp AS no_ktp, 
+	pasien.no_peserta AS no_peserta, 
+	pasien.nm_pasien AS nm_pasien, 
+	pasien.jk AS jk, 
+	pasien.agama AS agama, 
+	pasien.suku_bangsa AS suku_bangsa, 
+	pasien.pekerjaan AS pekerjaan, 
+	pasien.pnd AS pnd, 
+	pasien.tgl_lahir AS tgl_lahir, 
+	pasien.umur AS umur, 
+	pasien.alamat AS alamat, 
+	kelurahan.nm_kel AS nm_kel, 
+	kecamatan.nm_kec AS nm_kec, 
+	kabupaten.nm_kab AS nm_kab, 
+	propinsi.nm_prop AS nm_prop, 
+	pasien.no_tlp AS no_tlp, 
+	poliklinik.nm_poli AS nm_poli, 
+	dokter.nm_dokter AS nm_dokter, 
+	penjab.png_jawab AS png_jawab, 
+	penyakit.nm_penyakit AS nm_penyakit, 
+	diagnosa_pasien.kd_penyakit AS kd_penyakit
+FROM
+	(
+		(
+			(
+				(
+					(
+						(
+							(
+								(
+									(
+										(
+											reg_periksa
+											join
+											pasien
+											ON 
+												(
+													reg_periksa.no_rkm_medis = pasien.no_rkm_medis
+												)
+										)
+										join
+										penjab
+										ON 
+											(
+												reg_periksa.kd_pj = penjab.kd_pj
+											)
+									)
+									join
+									dokter
+									ON 
+										(
+											reg_periksa.kd_dokter = dokter.kd_dokter
+										)
+								)
+								join
+								diagnosa_pasien
+								ON 
+									(
+										reg_periksa.no_rawat = diagnosa_pasien.no_rawat
+									)
+							)
+							join
+							penyakit
+							ON 
+								(
+									diagnosa_pasien.kd_penyakit = penyakit.kd_penyakit
+								)
+						)
+						join
+						poliklinik
+						ON 
+							(
+								reg_periksa.kd_poli = poliklinik.kd_poli
+							)
+					)
+					join
+					kelurahan
+					ON 
+						(
+							pasien.kd_kel = kelurahan.kd_kel
+						)
+				)
+				join
+				kecamatan
+				ON 
+					(
+						pasien.kd_kec = kecamatan.kd_kec
+					)
+			)
+			join
+			propinsi
+			ON 
+				(
+					pasien.kd_prop = propinsi.kd_prop
+				)
+		)
+		join
+		kabupaten
+		ON 
+			(
+				pasien.kd_kab = kabupaten.kd_kab
+			)
+	)
+WHERE
+	reg_periksa.tgl_registrasi BETWEEN '2022-10-1' AND '2022-10-2' AND
+	reg_periksa.status_lanjut = 'Ralan'
+	`;
+    const result = await con.query(sql, [from, until]);
+    return result;
+};
+
+module.exports = {
+    getLaporan
+}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const base = require('../controllers');
 const auth = require('../controllers/AuthController');
 const dokter = require('../controllers/DokterController');
 const igd = require('../controllers/IgdController');
+const ranap = require('../controllers/RanapController');
+const ralan = require('../controllers/RalanController');
 
 const middleware = require('../middlewares');
 
@@ -17,5 +19,9 @@ routes.get('/pasien/registasiawal',middleware.login, dokter.pasien);
 routes.get('/pasien/rkm_medis',middleware.login, dokter.rkm_medis);
 routes.get('/obat/cari',middleware.login, dokter.getObat);
 routes.post('/igd/regis', middleware.login, igd.regis);
+// routes.post('/ranap/regis', middleware.login, ranap.regis);
+// routes.post('/ranap/laporan', middleware.login, ranap.getRegLaporan);
+// routes.post('/ralan/regis', middleware.login, igd.regis);
+routes.post('/ralan/laporan', middleware.login, ralan.getRegLaporan);
 
 module.exports = routes;
